febbc54de1fe98dbb147792835e8b9e7d5125e92
Nitin Srivastava <nitin.srivastava@gslab.com>
2022-08-29 10:22:36 +0530

    Intial commit
    uHost simple static website.

---
+++ shared.js
@@ -0,0 +1,38 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var modalNoButton = document.querySelector(".modal__action--negative");
+
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className='open' THIS WILL OVERRIDE THE COMPLETE CLASSLIST
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  backdrop.classList.remove("open");
+  if (modal) {
+    modal.classList.remove("open");
+  }
+}
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
